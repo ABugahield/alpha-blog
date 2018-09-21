a97405d04d21cefe45772b99579bdf234a2fcc6d
Karol <maruch600@gmail.com>
2018-09-21 12:56:29 +0000

    Complete authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user , only: [:edit, :update] 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -13,7 +14,6 @@ class UsersController < ApplicationController
   
   
   def create
-    
      @user = User.new(user_params)
      if @user.save
        flash[:success] = "Welcome to the alpha blog #{@user.username}"
@@ -24,12 +24,10 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
     
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params) 
       flash[:success] = "You account was updated successfully"
       redirect_to articles_path
@@ -40,7 +38,6 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
@@ -48,5 +45,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  def require_same_user
+    if  current_user != @user
+      flash[:danger] = "You can only edit your own account"
+    end
+  end
 end
